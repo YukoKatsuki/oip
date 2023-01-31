0b722dd39b03cdc3b6059285f3dfac7e261fdcbb
Zeionn <87084175+YukoKatsuki@users.noreply.github.com>
2023-01-31 19:50:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,69 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+var Textbox = document.getElementById("textbox");
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event) {
+
+    console.log(event);
+
+var Content = event.results[0][0].transcript;
+    console.log(Content);
+        if(Content =="tire minha selfie")
+        {
+            console.log("tirando selfie --- ");
+            speak();
+        }
+
+document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+}
+
+function speak() {
+
+    var synth = window.speechSynthesis;
+
+    speakData = "Tirando sua selfie em 5, 4, 3, 2, 1.";
+
+    var utterThis = new SpeechSynthesisUtterance(speakData);
+
+    synth.speak(utterThis);
+
+    Webcam.attach(camera);
+
+    setTimeout(function()
+    {
+        takeSelfie();
+        save();
+    }, 5000);
+}
+
+camera = document.getElementById("camera");
+Webcam.set({
+        width:360,
+        height:250,
+        image_format : 'jpeg',
+        jpeg_quality:90
+});
+
+function takeSelfie()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="selfieImage" src="'+data_uri+'"/>';
+    });
+}
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfieImage").src;
+    link.href = image;
+    link.click();
+}
